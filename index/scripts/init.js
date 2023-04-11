ad1ad7d0ff75bb3ab926ff1059bfba020730cc9c
Dyrda Lev <akronman0000@gmail.com>
2023-04-12 00:44:49 +0500

    [WIP] Верстка главной страницы

---
+++ index/scripts/init.js
@@ -0,0 +1,5 @@
+const bEmployee = document.getElementById('bEmployee');
+const bCompany = document.getElementById('bCompany');
+
+bEmployee.style.background = '#FF5A5F';
+bCompany.style.background = '#087E8B';

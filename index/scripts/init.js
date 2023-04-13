bdf3018850770d922c558d027c1dd90238ad29e3
Dyrda Lev <akronman0000@gmail.com>
2023-04-14 02:56:07 +0500

    Форма регистрации готова. Небольшие проблемы с подсвечиванием поля ввода после закрытия, пока хз как чинить

--- index/scripts/init.js
+++ index/scripts/init.js
@@ -3,3 +3,15 @@ const bCompany = document.getElementById('bCompany');
 
 bEmployee.style.background = '#FF5A5F';
 bCompany.style.background = '#087E8B';
+
+const inputs = document.querySelectorAll('.text-input');
+for (let i of inputs) {
+    i.addEventListener('blur', function () {
+        if (!(i.checkValidity())) {
+            i.classList.add('invalid');
+        }
+        else {
+            i.classList.remove('invalid');
+        }
+    })
+}

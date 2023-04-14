539339b4236e4cfb5b682d5945b69e73ae9fe83c
Dyrda Lev <akronman0000@gmail.com>
2023-04-15 04:34:24 +0500

    Вынес скрипты popup в init. сделал кнопку вход. добавил секцию с переходом в регистрацию

--- index/scripts/init.js
+++ index/scripts/init.js
@@ -15,3 +15,31 @@ for (let i of inputs) {
         }
     })
 }
+
+//
+// const ppReg = document.getElementById('reg-pop');
+// const ppAuth = document.getElementById('auth-pop');
+const ppBlur = document.getElementById('pp-bg');
+function ppShow (id) {
+    const pp = document.getElementById(id);
+    pp.style.visibility = 'visible';
+    pp.style.opacity = '1';
+
+    ppBlur.style.visibility = 'visible';
+}
+
+function ppHide(id) {
+    const pp = document.getElementById(id);
+    pp.style.visibility = 'hidden';
+    pp.style.opacity = '0';
+
+    ppBlur.style.visibility = 'hidden';
+    for (let child of pp.children) {
+        if (child.className === 'input-line') {
+            for (let c of child.children) {
+                if (c.tagName === 'INPUT')
+                    c.value = '';
+            }
+        }
+    }
+}

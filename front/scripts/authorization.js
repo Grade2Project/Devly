8831eeb0ea577392dd3dd6f69eee5f436b7ee1c5
m.shkut <m.shkut@tinkoff.ru>
2023-06-12 19:39:40 +0500

    auth fixes

--- front/scripts/authorization.js
+++ front/scripts/authorization.js
@@ -1,4 +1,4 @@
-function auth() {
+function authorize() {
     const dataRaw = getObjectFromIterable(
         document.querySelectorAll('[id^="auth_"]'),
         ti => [ti.id, ti.value]);
@@ -28,9 +28,5 @@ function auth() {
 
 function startWrongPasswordAnimation() {
     let passwordField = document.querySelector('input[id=auth_password]');
-    passwordField.style.animationName = "";
-    passwordField.offsetHeight;
-    passwordField.style.animationName = "shake, glow-red";
-    passwordField.style.animationDuration = "0.7s, 0.35s";
-    passwordField.style.animationIterationCount = "1, 2";
+    incorrectInputAnimation(passwordField);
 }

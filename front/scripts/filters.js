3db65b3d81002009c69fefdfedbf7b0a8ea98da9
Dyrda Lev <akronman0000@gmail.com>
2023-06-14 22:47:21 +0500

    Минорные фиксы

--- front/scripts/filters.js
+++ front/scripts/filters.js
@@ -1,5 +1,3 @@
-//TODO: сделать единый environment для всех хендлеров, фильтров и прочего.
-
 class Filter {
     __container = document.querySelector('.content__container');
     __filer__template = document.getElementById('filter__base');
@@ -143,23 +141,6 @@ class CompanyFilters extends Filters {
 
 let token = localStorage.getItem('token');
 
-// btnApplyFilters.onclick = () => {
-//     cardHandler.applyFilters({
-//         grades: filters.grades.length !== 0 ? filters.grades : null,
-//         languages: filters.languages.length !== 0 ? filters.languages : null,
-//         experienceFrom: 0,
-//         city: null,
-//         userName: null
-//     });
-// }
-//
-// function isFiltersEmpty() {
-//     return filters.languages.length === 0
-//         && filters.grades.length === 0
-//         && filters.city.length === 0
-//         && filters.experienceFrom === 0;
-// }
-//
 // let changeHandler = {
 //     set: function (target, key, value) {
 //         target[key] = value;
@@ -182,42 +163,3 @@ let token = localStorage.getItem('token');
 //     city: [],
 //     experienceFrom: 0
 // }, changeHandler);
-//
-// fetchFrom(Controllers.GRADES, (statusCode, response) => {
-//     let grades = response.container;
-//     let gradesFilter = sellect(document.getElementById('grades__filter'), {
-//         originList: grades,
-//         onInsert: () => {
-//             filters.grades = [...gradesFilter.getSelected()];
-//         }
-//     });
-//
-//     gradesFilter.init();
-// }, localStorage['token']);
-//
-// fetchFrom(Controllers.LANGS, (statusCode, response) => {
-//     let langs = response.container;
-//     let langsFilter = sellect(document.getElementById('languages__filter'), {
-//         originList: langs,
-//         onInsert: () => {
-//             filters.languages = [...langsFilter.getSelected()];
-//         }
-//     });
-//
-//     langsFilter.init();
-// }, localStorage['token']);
-//
-// let citiesFilterE = document.getElementById('cities__filter');
-// let citiesFilter = sellect(citiesFilterE, {
-//     originList: [],
-//     onInsert: () => {
-//         filters.city = [...citiesFilter.getSelected()];
-//     }
-// });
-// citiesFilter.init();
-//
-// citiesFilterE.oninput = () => {
-//     fetchFrom(`${Controllers.CITIES}?pattern=${citiesFilterE.value}`, (statusCode, response) => {
-//         citiesFilter.refresh(response.container);
-//     });
-// }

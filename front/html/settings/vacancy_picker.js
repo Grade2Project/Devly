5b4ad9790bb7cf7b1bb1f666c3a69cd992b6d416
Dyrda Lev <akronman0000@gmail.com>
2023-06-21 02:03:10 +0500

    Настройки для пользователя и hr готовы, немного изменил структуру файлов, поменял стили и заполнение(марк сделай рефакторинг). Не тестил в общем случае потому что хочу спать...

---
+++ front/html/settings/vacancy_picker.js
@@ -0,0 +1,17 @@
+const __holder = document.querySelector('.vacancies__picker');
+const __template__card = document.getElementById('vacancy__element__template');
+const __template__empty = document.getElementById('vacancy__template__empty');
+class VacancyElement {
+    constructor(vacancyObj) {
+        let vcard = __template__card.content.cloneNode(true);
+        let vinfo = JSON.parse(vacancyObj['info']);
+
+        vcard.getElementById('vacancy__position').innerText = vinfo['position'];
+        vcard.getElementById('vacancy__experience').innerText = vinfo['experience'];
+        vcard.getElementById('vacancy__grade').innerText = vacancyObj['grade'];
+        vcard.getElementById('vacancy__language').innerText = vacancyObj['programmingLanguage'];
+        vcard.getElementById('vacancy__salary').innerText = vacancyObj['salary'];
+
+        __holder.appendChild(vcard);
+    }
+}

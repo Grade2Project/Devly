070d7fbd9496a39087518a1e6fab515bbc4ad8eb
Dyrda Lev <akronman0000@gmail.com>
2023-06-21 22:08:09 +0500

    Создание вакансий работает. Удаление вакансий работает. Вакансии разложены в неправильном порядке 0.0

--- front/html/settings/vacancy_picker.js
+++ front/html/settings/vacancy_picker.js
@@ -13,10 +13,19 @@ class VacancyElement {
         vcard.getElementById('vacancy__salary').innerText = vacancyObj['salary'];
 
         vcard.querySelector('.remove__button').onclick = (e) => {
-            e.target.parentElement.classList.add('removed');
-            e.target.parentElement.onanimationend = (e) => {
-                e.target.remove();
-            }
+            sendJSON(vacancyObj['id'],
+                Controllers.VACANCY.DELETE, HTTPResponseType.TEXT,
+                (status, response) => {
+                if (status === 200) {
+                    e.target.parentElement.classList.add('removed');
+                    e.target.parentElement.onanimationend = (e) => {
+                        e.target.remove();
+                    }
+                }
+                else {
+                    alert('ОШИБКА');
+                }
+            }, localStorage['token']);
         }
 
         __holder.appendChild(vcard);
